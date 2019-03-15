668af61836b5de3785e35bad9daa75b55a231b97
Yason <yheissoneduardo19@gmail.com>
2019-03-14 23:34:16 -0400

    Agremagos tabla categories y empezamos a migrar en la BD}

--- database/migrations/2019_03_15_025202_add_categories_table.php
+++ database/migrations/2019_03_15_025202_add_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->enum('type', ['member', 'admin'])->default('member');
-            $table->rememberToken();
+            $table->string('name', 60);
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('categories');
     }
 }
