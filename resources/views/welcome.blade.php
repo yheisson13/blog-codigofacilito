1b24d9cb3f09b94982807ce5a3816b87368c6ac3
Yason <yheissoneduardo19@gmail.com>
2019-03-14 22:10:38 -0400

    Modificado la vista del wellcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Codigo Facilito</div>
             </div>
         </div>
     </body>
